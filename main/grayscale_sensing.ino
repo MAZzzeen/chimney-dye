8430d5ba1557213a1540670a6348c92ed74a307c
MAZzzeen <104923042+MAZzzeen@users.noreply.github.com>
2022-05-05 08:30:04 +0200

    Update grayscale_sensing.ino

--- main/grayscale_sensing.ino
+++ main/grayscale_sensing.ino
@@ -1,16 +1,20 @@
+//mapping pins
 #define S0 2
 #define S1 3
 #define S2 4
 #define S3 5
 #define sensorOut 6
 
+//calibration
 int whiteMin = 66;
 int whiteMax = 282;
 
+//making variables
 int colordeg = 0;
 
 int whiteValue;
 
+//setting pin modes
 void setup() {
  
   pinMode(S0, OUTPUT);
@@ -22,11 +26,13 @@ void setup() {
   
   digitalWrite(S0,HIGH);
   digitalWrite(S1,LOW);
-  
+ 
+ //starting serial
   Serial.begin(57600);
 }
 
 void loop() {
+ //getting values
   colordeg = getcolordeg();
   whiteValue = map(colordeg, whiteMin, whiteMax, 255, 0);
   delay(200);
